25b7535b016e4ea9fe2f3719df9bc1e03f2604c7
yousefhmza <ysfhmza10@gmail.com>
2024-09-04 21:34:29 +0300

    The Movies app task

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,15 +1,10 @@
 class Constants {
   static const String empty = "";
-  static const String appName = "Kreya driver";
+  static const String appName = "Movies";
   static const int defaultPaginationLimit = 10;
   static const String appDomain = "https://api.themoviedb.org/3";
   static const String imagesBaseUrl = "https://image.tmdb.org/t/p/w500";
   static const String apiKey = "b5e054dad2220d8da2a7b973949d0d57";
-  static const String apiReadAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWUwNTRkYWQyMjIwZDhkYTJhN2I5NzM5NDlkMGQ1NyIsIm5iZiI6MTcyNTQ1MzQ1Mi40MTg3NTQsInN1YiI6IjY2ZDg1M2QzMzA2ZDU4YWFiNzYwNGIwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CNiL5R9EAxEXsrFjaS0GFHENd6s3wq70K4eAq7bueig";
-}
-
-class FirebaseConstants {
-  static const String driversRef = "drivers";
-  static const String tripRequestsRef = "trip_requests";
-  static const String tripsRef = "trips";
+  static const String apiReadAccessToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWUwNTRkYWQyMjIwZDhkYTJhN2I5NzM5NDlkMGQ1NyIsIm5iZiI6MTcyNTQ1MzQ1Mi40MTg3NTQsInN1YiI6IjY2ZDg1M2QzMzA2ZDU4YWFiNzYwNGIwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CNiL5R9EAxEXsrFjaS0GFHENd6s3wq70K4eAq7bueig";
 }
